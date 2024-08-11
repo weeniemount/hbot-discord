4471922747a7422a795ed06d7b2e0c60ab2678e4
weeniemount <59868100+weeniemount@users.noreply.github.com>
2024-08-11 20:16:38 +0200

    fix dancing h command

--- commands/fun/dancingh.js
+++ commands/fun/dancingh.js
@@ -1,11 +1,15 @@
+const { channel } = require('diagnostics_channel');
 const { SlashCommandBuilder } = require('discord.js');
-const file = new AttachmentBuilder('../assets/discordjs.png');
+const { EmbedBuilder, AttachmentBuilder } = require('discord.js');
 
+const file = new AttachmentBuilder('images/dancingh.gif');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('dancingh')
 		.setDescription('send a dancing h in chat'),
 	async execute(interaction) {
-		await interaction.reply();
+		const exampleEmbed = new EmbedBuilder()
+			.setImage('attachment://dancingh.gif');
+		await interaction.reply({ embeds: [exampleEmbed], files: [file] });
 	},
 };
