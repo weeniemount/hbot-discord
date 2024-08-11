51fdb318443f3ceb47cfd9061d6959357cec7a81
weeniemount <59868100+weeniemount@users.noreply.github.com>
2024-08-11 22:04:16 +0200

    Update dancingletters.js

--- commands/fun/dancingletters.js
+++ commands/fun/dancingletters.js
@@ -11,7 +11,14 @@ module.exports = {
 		.setDescription('the letter you want')
 		.setRequired(true)),
 		async execute(interaction) {
-			const file = new AttachmentBuilder(`images/dancingletters/${interaction.options.getString('letter')}.gif`);
-			await interaction.reply({ files: [file] });
+			if (interaction.options.getString('letter').length < 2) {
+				const file = new AttachmentBuilder(`images/dancingletters/${interaction.options.getString('letter')}.gif`);
+				await interaction.reply({ files: [file] });
+			} else if (interaction.options.getString('letter').length >= 2) {
+				const errorembed = new EmbedBuilder()
+					.setColor(0xef2213)
+					.setTitle('you cant put something longer then 1 character!')
+				await interaction.reply({ embeds: [errorembed], ephemeral: true });
+			}
 	},
 };
