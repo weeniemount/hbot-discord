1232c27570ac21768ddf87496939776228ba009d
weeniemount <59868100+weeniemount@users.noreply.github.com>
2024-08-11 20:39:46 +0200

    help.js and pfp added

---
+++ commands/util/help.js
@@ -0,0 +1,22 @@
+const { channel } = require('diagnostics_channel');
+const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder, AttachmentBuilder } = require('discord.js');
+
+const file = new AttachmentBuilder('images/pfp.png');
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('help')
+		.setDescription('h bot commands!'),
+	async execute(interaction) {
+		const exampleEmbed = new EmbedBuilder()
+            .setColor(0xef2213)
+            .setTitle('h bot help')
+            .setAuthor({ name: 'h bot', iconURL: 'attachment://pfp.png'})
+            .setThumbnail('attachment://pfp.png')
+            .addFields(
+                { name: 'funnys', value: '/dancingh - shows you a dancing h gif\n/ping - pong!' },
+                { name: 'useful', value: '/help - shows you this help page' },
+            )
+		await interaction.reply({ embeds: [exampleEmbed], files: [file] });
+	},
+};
