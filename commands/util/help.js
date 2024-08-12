4f9d58627dc4f2d5f1e3fa3e4ccd6c15bf643f22
weeniemount <59868100+weeniemount@users.noreply.github.com>
2024-08-12 21:43:45 +0200

    updated help page

--- commands/util/help.js
+++ commands/util/help.js
@@ -17,6 +17,7 @@ module.exports = {
             .addFields(
                 { name: 'funnys', value: '/dancingh - shows you a dancing h gif\n/dancing <letter/number/symbol> - show a dancing letter, number or smybol of your choice\n/dancingwords <words> - make your words out of dancing letters!\n/ping - pong!' },
                 { name: 'useful', value: '/help - shows you this help page' },
+                { name: 'currency', value: '/hointsbalance - shows you your hoints balance\n/hointgiver - gives you one single hoint' },
             )
 		await interaction.reply({ embeds: [exampleEmbed], files: [file], ephemeral: true });
 	},
