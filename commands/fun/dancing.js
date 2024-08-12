73d28bd4a9c2f2672df1722a7a90d5996fd168f7
weeniemount <59868100+weeniemount@users.noreply.github.com>
2024-08-12 11:45:44 +0200

    symbol support and extra error handling

--- commands/fun/dancing.js
+++ commands/fun/dancing.js
@@ -7,14 +7,36 @@ module.exports = {
 	.setName('dancing')
 	.setDescription('send a dancing letter, number or symbol of your choice in chat')
 	.addStringOption(option =>
-		option.setName('letter')
-		.setDescription('the letter you want')
+		option.setName('input')
+		.setDescription('the letter, number or symbol you want')
 		.setRequired(true)),
 		async execute(interaction) {
-			if (interaction.options.getString('letter').length < 2) {
-				const file = new AttachmentBuilder(`images/dancingletters/${interaction.options.getString('letter')}.gif`);
-				await interaction.reply({ files: [file] });
-			} else if (interaction.options.getString('letter').length >= 2) {
+			const letterRegex = /^[a-zA-Z]+$/;
+  			const numberRegex = /^[0-9]+$/;
+			var inputted = interaction.options.getString('input')
+			var symbolfilename = {
+				"&": "ampersand",
+				"@": "at",
+				"$": "dollarsign",
+				"!": "exclamationpoint",
+				"?": "questionmark"
+			}
+			if (inputted.length < 2) {
+				if (inputted == "&" || inputted == "@" || inputted == "$" || inputted == "!" || inputted == "?") {
+					const file = new AttachmentBuilder(`images/dancingletters/${symbolfilename[inputted]}.gif`);
+					await interaction.reply({ files: [file] });
+				} else {
+					if (letterRegex.test(inputted) || numberRegex.test(inputted)) {
+						const file = new AttachmentBuilder(`images/dancingletters/${inputted}.gif`);
+						await interaction.reply({ files: [file] });
+					} else {
+						const errorembed = new EmbedBuilder()
+							.setColor(0xef2213)
+							.setTitle('that symbol isnt allowed!')
+						await interaction.reply({ embeds: [errorembed], ephemeral: true });
+					}
+				}
+			} else if (inputted.length >= 2) {
 				const errorembed = new EmbedBuilder()
 					.setColor(0xef2213)
 					.setTitle('you cant put something longer then 1 character!')
