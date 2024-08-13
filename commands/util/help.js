752b33f2b1efce7bf29cb7f010d3d1bb11ba2128
weeniemount <59868100+weeniemount@users.noreply.github.com>
2024-08-13 12:43:03 +0200

    added myself as a footer

--- commands/util/help.js
+++ commands/util/help.js
@@ -4,6 +4,7 @@ const { EmbedBuilder, AttachmentBuilder } = require('discord.js');
 const emojiids = require('../../modules/emojiids.js')
 
 const file = new AttachmentBuilder('images/pfp.png');
+const weeniemount = new AttachmentBuilder(`images/weeniemount.png`);
 
 module.exports = {
 	data: new SlashCommandBuilder({ integration_types: [0,1] })
@@ -18,8 +19,9 @@ module.exports = {
             .addFields(
                 { name: 'funnys', value: '/dancingh - shows you a dancing h gif\n/dancing <letter/number/symbol> - show a dancing letter, number or smybol of your choice\n/dancingwords <words> - make your words out of dancing letters!\n/ping - pong!' },
                 { name: 'useful', value: '/help - shows you this help page' },
-                { name: `currency <:hoint:${emojiids["hoint"]}>`, value: '/hointsbalance - shows you your hoints balance\n/hointgiver - gives you one single hoint\n/hointsgamble - get 1 - 50 hoints\n/hointleaderboard - top 10 people with the most hoints' },
+                { name: `currency`, value: '/hointsbalance - shows you your hoints balance\n/hointgiver - gives you one single hoint\n/hointsgamble - get 1 - 50 hoints\n/hointleaderboard - top 10 people with the most hoints' },
             )
-		await interaction.reply({ embeds: [exampleEmbed], files: [file], ephemeral: true });
+            .setFooter({ text: `made with love and h by @weeniemount`, iconURL: 'attachment://weeniemount.png' });
+		await interaction.reply({ embeds: [exampleEmbed], files: [file,weeniemount], ephemeral: true });
 	},
 };
