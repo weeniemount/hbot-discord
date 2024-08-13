d5d4b804440027209ae8b05afc6a7fa64977ac6e
weeniemount <59868100+weeniemount@users.noreply.github.com>
2024-08-13 17:34:10 +0200

    submmtin' h facts to the db

--- commands/hfacts/submithfact.js
+++ commands/hfacts/submithfact.js
@@ -2,7 +2,12 @@ const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRow
 const hfactstable = require('../../modules/hfactstable.js')
 const { token } = require('../../config.json');
 const client = require('../../modules/client.js')
+const database = require('../../modules/database.js')
 
+var userid
+var username
+var hfact
+var userpfp
 
 module.exports = {
 	data: new SlashCommandBuilder({ integration_types: [0,1] })
@@ -14,6 +19,10 @@ module.exports = {
             .setRequired(true)),
 	async execute(interaction) {
         const randomFact = Math.floor(Math.random() * hfactstable.length);
+        userid = interaction.user.id
+        username = interaction.user.tag
+        hfact = interaction.options.getString('fact')
+        userpfp = interaction.user.avatarURL()
         const fact = new EmbedBuilder()
             .setColor(0xef2213)
             .setTitle("your h fact has been submitted")
@@ -39,9 +48,38 @@ module.exports = {
                 .setColor(0xef2213)
                 .setTitle('h fact submission')
                 .addFields(
-                    { name: 'h fact:', value: interaction.options.getString('fact') },
+                    { name: 'h fact:', value: hfact },
                 )
-            await channel.send({ embeds: [embed], components: [row], });
+                .setFooter({text: `h fact submmited by @${username}`, iconURL: userpfp})
+            const response = await channel.send({ embeds: [embed], components: [row], });
+            //const collectorFilter = 635875339804868638;
+
+            try {
+                const confirmation = await response.awaitMessageComponent({ time: 28800 });
+                if (confirmation.customId === 'accept') {
+                    const embedaccept = new EmbedBuilder()
+                        .setColor(0xef2213)
+                        .setTitle('h fact submission')
+                        .addFields(
+                            { name: `h fact "${hfact}" has been accepted`, value: 'yey!' },
+                        )
+                        .setFooter({text: `h fact submmited by @${username}`, iconURL: userpfp})
+                    await confirmation.update({ embeds: [embedaccept], components: [] });
+                    database.prepare('INSERT INTO submittedhfacts (hfact, userid, username) VALUES (?, ?, ?)').run(hfact, userid, username);
+                } else if (confirmation.customId === 'decline') {
+                    const embeddecline = new EmbedBuilder()
+                        .setColor(0xef2213)
+                        .setTitle('h fact submission')
+                        .addFields(
+                            { name: `h fact ${hfact} has been declined`, value: 'aww...' },
+                        )
+                        .setFooter({text: `h fact submmited by @${username}`, iconURL: userpfp})
+                    await confirmation.update({ embeds: [embeddecline], components: [] });
+                }
+            } catch (e) {
+                console.log(`h fact '${hfact}' has been declined because no one accepted/declined it. sad....`)
+                //await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
+            }
         } else {
             console.error("Channel not found or is not a text-based channel.");
         }
--- modules/database.js
+++ modules/database.js
@@ -2,6 +2,7 @@ const Database = require('better-sqlite3');
 const database = new Database('hbotdatabase.db');
 
 database.prepare('CREATE TABLE IF NOT EXISTS users (id TEXT PRIMARY KEY, username TEXT, hoints INTEGER)').run();
+database.prepare('CREATE TABLE IF NOT EXISTS submittedhfacts (hfact TEXT PRIMARY KEY, userid TEXT, username TEXT)').run();
 //database.prepare('CREATE TABLE IF NOT EXISTS servers (id TEXT PRIMARY KEY, lyntrenabled INTEGER, lyntrchannelid INTEGER)').run();
 
 module.exports = database;
