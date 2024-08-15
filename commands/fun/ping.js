f1eaea367cc44a50bb294be0fd876408cb67c423
weeniemount <59868100+weeniemount@users.noreply.github.com>
2024-08-15 10:16:31 +0200

    some stuf

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder({ integration_types: [0,1] })
 		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setDescription('replies with Pong!'),
 	async execute(interaction) {
 		await interaction.reply('Pong!');
 	},
