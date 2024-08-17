2281a7be59efa95aba91eed97e93a04185e47175
weeniemount <59868100+weeniemount@users.noreply.github.com>
2024-08-17 20:17:05 +0200

    this code sucks ass

--- commands/currency/hointshop.js
+++ commands/currency/hointshop.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js');
 const emojiids = require('../../modules/emojiids.js')
+const items = require('../../modules/items.js')
 const { guildId } = require('../../config.json')
 
 module.exports = {
@@ -10,20 +11,22 @@ module.exports = {
         const currentserverid = interaction.guild.id
 
         const page1button = new ButtonBuilder()
-            .setCustomId(`page1`)
-            .setLabel(`page1`)
+            .setCustomId(`page1button`)
+            .setLabel(`page 1`)
             //.setEmoji(emojiids["steamhappy"])
             .setStyle(ButtonStyle.Primary);
         
         
         const row = new ActionRowBuilder()
             .addComponents(page1button);
-        
+        const page1items = items.slice(0,5)
         const page1 = new EmbedBuilder()
             .setColor(0xef2213)
             .setTitle(`<:info:${emojiids["info"]}> hoint shop - page 1`)
             .setThumbnail('attachment://pfp.png')
-            .addFields({ name: `swaglies`, value: 'can' })
+            .addFields(
+                { name: `<:${page1items[0].itemid}:${page1items[0].emoji}> ${page1items[0].name} : ${page1items[0].price} <:hoint:${emojiids["hoint"]}>`, value: `${page1items[0].description}` },
+            )
         const updateInteraction = async (interaction) => {
             const filter = i => i.user.id === interaction.user.id;
             const collector = interaction.channel.createMessageComponentCollector({ filter, time: 1200000 });
---
+++ modules/items.js
@@ -0,0 +1,6 @@
+const emojiids = require("./emojiids.js")
+const items = [
+    {itemid: "swaglies", "name": "swaglies", "description": "random candy brand.", "price": "10", "emoji": `${emojiids["swaglies"]}`},
+]
+
+module.exports = items;
