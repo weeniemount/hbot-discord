877cb6238d6583974b082f96370f0ad1c293e0da
weeniemount <59868100+weeniemount@users.noreply.github.com>
2024-08-17 17:36:06 +0200

    send hoints confirmation

--- commands/currency/sendhoints.js
+++ commands/currency/sendhoints.js
@@ -1,4 +1,4 @@
-const {SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const {SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
 const database = require('../../modules/database.js');
 const emojiids = require('../../modules/emojiids.js')
 
@@ -22,21 +22,7 @@ module.exports = {
         const userSend = interaction.options.getUser('user')
         const hointstosend = interaction.options.getInteger('hoints')
         const user = database.prepare('SELECT * FROM users WHERE id = ?').get(userSend.id);
-        if (user) {
-            //console.log(`user ${userSend.username} already exists in the database! we can send h points to them!`)
-            if (hointstosend < 0 || userSend.id == userId || database.prepare('SELECT hoints FROM users WHERE id = ?').get(userId).hoints < hointstosend) {
-                void(0);
-            } else {
-                database.prepare('UPDATE users SET hoints = hoints - ? WHERE id = ?').run(hointstosend, userId);
-                database.prepare('UPDATE users SET hoints = hoints + ? WHERE id = ?').run(hointstosend, userSend.id);
-            }
-        } else {
-            //console.log(`user ${userSend.username} doesnt exist in the database! cant send h points to them!`)
-        }
-        
-        const sentEmbed = new EmbedBuilder()
-            .setColor(0xef2213)
-            .setTitle(`you have sent ${hointstosend} <:hoint:${emojiids["hoint"]}> hoints to user "${userSend.globalName}"!`)
+
         if (hointstosend < 0) {
             const errorembed = new EmbedBuilder()
                 .setColor(0xef2213)
@@ -58,11 +44,57 @@ module.exports = {
         if (!user) {
             const sentembed = new EmbedBuilder()
                 .setColor(0xef2213)
-                .setTitle(`that user doent have a hoint balance!`)
+                .setTitle(`that user doesnt have a hoint balance!`)
             return await interaction.reply({ embeds: [sentembed], ephemeral: true});
         }
-        if (user || userSend.id != userId || hointstosend > 0) 
-            {await interaction.reply({ embeds: [sentEmbed], ephemeral: true}); 
+
+        const yes = new ButtonBuilder()
+			.setCustomId('yes')
+			.setLabel('yes')
+			.setStyle(ButtonStyle.Success);
+        const no = new ButtonBuilder()
+			.setCustomId('no')
+			.setLabel('no')
+			.setStyle(ButtonStyle.Danger);
+
+        const row = new ActionRowBuilder()
+			.addComponents(yes, no);
+        
+        const confirmembed = new EmbedBuilder()
+            .setColor(0xef2213)
+            .setTitle(`are you sure you want to send ${hointstosend} hoints <:hoint:${emojiids["hoint"]}> to ${userSend.globalName}?`)
+
+        const response = await interaction.reply({ embeds: [confirmembed], components: [row], ephemeral: true });
+        const confirmation = await response.awaitMessageComponent({ time: 60000 });
+
+        try {
+            if (confirmation.customId === 'yes') {
+                if (user) {
+                    //console.log(`user ${userSend.username} already exists in the database! we can send h points to them!`)
+                    if (hointstosend < 0 || userSend.id == userId || database.prepare('SELECT hoints FROM users WHERE id = ?').get(userId).hoints < hointstosend) {
+                        void(0);
+                    } else {
+                        database.prepare('UPDATE users SET hoints = hoints - ? WHERE id = ?').run(hointstosend, userId);
+                        database.prepare('UPDATE users SET hoints = hoints + ? WHERE id = ?').run(hointstosend, userSend.id);
+                    }
+                }// else {
+                    //console.log(`user ${userSend.username} doesnt exist in the database! cant send h points to them!`)
+                //}
+                
+                const sentEmbed = new EmbedBuilder()
+                    .setColor(0xef2213)
+                    .setTitle(`you have sent ${hointstosend} hoints <:hoint:${emojiids["hoint"]}> to user ${userSend.globalName}!`)
+                if (user || userSend.id != userId || hointstosend > 0) {
+                    await confirmation.update({ embeds: [sentEmbed], components: [], ephemeral: true});
+                }
+            } else if (confirmation.customId === 'no') {
+                const cancelembed = new EmbedBuilder()
+                    .setColor(0xef2213)
+                    .setTitle(`transaction of ${hointstosend} hoints <:hoint:${emojiids["hoint"]}> to ${userSend.globalName} cancelled.`)
+                await confirmation.update({ embeds: [cancelembed], components: [], ephemeral: true}); 
+            }
+        } catch(e) {
+            // h
         }
 	},
 };
