7fba235034643d960cdf903954466a2c75fab6e5
weeniemount <59868100+weeniemount@users.noreply.github.com>
2024-08-17 20:41:50 +0200

    a

--- commands/currency/buyitem.js
+++ commands/currency/buyitem.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js');
 const emojiids = require('../../modules/emojiids.js')
+const database = require('../../modules/database.js')
 const items = require('../../modules/items.js')
 
 module.exports = {
@@ -12,15 +13,33 @@ module.exports = {
             .setRequired(true)),
 	async execute(interaction) {
         const itemtobuy = interaction.options.getString('item')
+        const userid = interaction.user.id
+        const username = interaction.user.tag
+        const userhoints = database.prepare('SELECT * FROM users WHERE id = ?').get(userid);
+        const userinventory = database.prepare('SELECT * FROM inventory WHERE userid = ?').get(userid);
+
+        var inventorydata = []
+        if (userhoints) {
+            //console.log(`user ${interaction.user.tag} already exists in the database!`)
+        } else {
+            database.prepare('INSERT INTO users (id, username, hoints) VALUES (?, ?, ?)').run(userid, username, 0);
+        }
+
+        if (userinventory) {
+            inventorydata = JSON.parse(userinventory.inventorydata)
+        } else {
+            database.prepare('INSERT INTO inventory (userid, username, inventorydata) VALUES (?, ?, ?)').run(userid, username, JSON.stringify(inventorydata));
+        }
+
         var itemfound = false
         items.forEach(row => {
             if (row.name == itemtobuy) {
                 itemfound = true
-                console.log("item in le table")
+                await interaction.reply("i think i added an item to your inventory? im not sure check the db file lmfao")
             }
         });
         if (!itemfound) {
-            console.log("that item aint there wtf")
+            //console.log("that item aint there wtf")
         }
 	},
 };
--- modules/database.js
+++ modules/database.js
@@ -4,6 +4,7 @@ const database = new sqlite3db('hbotdatabase.db');
 database.prepare('CREATE TABLE IF NOT EXISTS users (id TEXT PRIMARY KEY, username TEXT, hoints INTEGER)').run();
 database.prepare('CREATE TABLE IF NOT EXISTS submittedhfacts (hfact TEXT PRIMARY KEY, userid TEXT, username TEXT)').run();
 database.prepare('CREATE TABLE IF NOT EXISTS hbotinfo (commandsran INTEGER PRIMARY KEY DEFAULT 0)').run();
+database.prepare('CREATE TABLE IF NOT EXISTS inventory (userid INTEGER PRIMARY KEY, username STRING, inventorydata STRING)').run();
 //database.prepare('CREATE TABLE IF NOT EXISTS servers (id TEXT PRIMARY KEY, lyntrenabled INTEGER, lyntrchannelid INTEGER)').run();
 
 const hbotinforecord = database.prepare('SELECT commandsran FROM hbotinfo LIMIT 1').get();
